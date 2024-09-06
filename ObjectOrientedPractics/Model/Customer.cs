ad2c2339b7d8f135f866dee6c764af7d60645ec9
Kirill Dvornikov <dvornikovkd@yandex.ru>
2024-09-06 11:53:52 +0700

    Add class Customer

---
+++ ObjectOrientedPractics/Model/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class Customer
+    {
+        private readonly int _id;
+        private string _fullname;
+        private string _address;
+
+        public int Id { get { return _id; } }
+        public string FullName 
+        { 
+            get { return _fullname; } 
+            set { _fullname = value; }
+        }
+        public string Address
+        {
+            get { return _address; }
+            set { _address = value; }
+        }
+        public Customer(string fullname, string address)
+        { 
+            FullName = fullname;
+            Address = address;
+        }
+    }
+}
