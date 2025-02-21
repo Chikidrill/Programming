b6424e96e7250516ae73e9c60af8d4b4bf1f969b
Chikidrill <dvornikovkd@yandex.ru>
2025-02-21 17:40:50 +0700

    feature(ViewModel): создан класс MainVM

---
+++ Contacts/ViewModel/MainVM.cs
@@ -0,0 +1,66 @@
+﻿using Contacts.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contacts.ViewModel
+{
+    public class MainVM: INotifyPropertyChanged
+    {
+        private Contact _contact;
+        public string Name
+        {
+            get => _contact.Name;
+            set
+            {
+                if (_contact.Name != value)
+                {
+                    _contact.Name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => _contact.PhoneNumber;
+            set
+            {
+                if (_contact.PhoneNumber != value)
+                {
+                    _contact.PhoneNumber = value;
+                    OnPropertyChanged(nameof(PhoneNumber));
+                }
+            }
+        }
+
+        public string Email
+        {
+            get => _contact.Email;
+            set
+            {
+                if (_contact.Email != value)
+                {
+                    _contact.Email = value;
+                    OnPropertyChanged(nameof(Email));
+                }
+            }
+        }
+
+        public MainVM()
+        {
+            _contact = new Contact();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
