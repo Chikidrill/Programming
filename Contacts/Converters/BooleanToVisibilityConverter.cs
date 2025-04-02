2354eb44670d55df5a1bfd9043ba911b1f02f98e
Chikidrill <dvornikovkd@yandex.ru>
2025-04-02 22:39:22 +0700

    feature(View): добавлен конвертер значений для изменения видимости кнопки Apply

---
+++ Contacts/Converters/BooleanToVisibilityConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Contacts.Converters;
+public class BooleanToVisibilityConverter: IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        if (value is bool boolValue)
+        {
+            return boolValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+        return Visibility.Collapsed;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
